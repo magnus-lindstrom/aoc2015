84010789e31dfc849879a45351e2e24c76c9fac6
Magnus Lindstrom <magnus.lindstrom@tuta.io>
2023-01-06 22:00:14 +0100

    add day13

--- src/13.lua
+++ src/13.lua
@@ -30,32 +30,95 @@ function permute(set, level, N, k)
 	set[k] = 0
 end
 
-local function get_happiness_sum(happiness_table, indexes)
+local function get_happiness_sum(happiness_table, indexes, nr_to_name)
 	local sum = 0
-	for p in permutations(#happiness_table-1) do
-		for i = 1, #happiness_table-2 do
-			sum = sum + happiness_table
+	local person, next_person
+	for i = 1, u.table_length(happiness_table)-1 do
+		person = nr_to_name[indexes[i]]
+		next_person = nr_to_name[indexes[i+1]]
+		sum = sum + happiness_table[person][next_person] + happiness_table[next_person][person]
+	end
+	-- same for person 1 and 8
+	person = nr_to_name[indexes[1]]
+	next_person = nr_to_name[indexes[u.table_length(indexes)]]
+	sum = sum + happiness_table[person][next_person] + happiness_table[next_person][person]
 
+	return sum
+end
+
+local function a()
+	local happiness_table = {}
+	for _, line in ipairs(input) do
+		local words = u.split_string_by_substring(line, ' ')
+		local person1 = words[1]
+		local person2 = words[11]:sub(1, -2)
+		local happiness = tonumber(words[4])
+		if happiness_table[person1] == nil then
+			happiness_table[person1] = {}
+		end
+
+		if words[3] == "gain" then
+			happiness_table[person1][person2] = happiness
+		else
+			happiness_table[person1][person2] = -1 * happiness
 		end
 	end
-end
-
-local happiness_table = {}
-for _, line in ipairs(input) do
-	local words = u.split_string_by_substring(line, ' ')
-	local person1 = words[1]
-	local person2 = words[11]:sub(1, -2)
-	local happiness = tonumber(words[4])
-	if happiness_table[person1] == nil then
-		happiness_table[person1] = {}
+	local nr_to_name = {}
+	local i = 1
+	for k, _ in pairs(happiness_table) do
+		nr_to_name[i] = k
+		i = i + 1
 	end
 
-	happiness_table[person1][person2] = happiness
-end
-print(u.get_table_string(happiness_table))
-
-for p in permutations(#happiness_table - 1) do
-	for k, v in pairs(p) do
-		print(k, v)
+	local max_happiness = 0
+	for p in permutations(u.table_length(happiness_table)) do
+		local happiness = get_happiness_sum(happiness_table, p, nr_to_name)
+		if happiness > max_happiness then
+			max_happiness = happiness
+		end
 	end
+
+	print('13a:', max_happiness)
 end
+
+local function b()
+	local happiness_table = {}
+	happiness_table['me'] = {}
+
+	for _, line in ipairs(input) do
+		local words = u.split_string_by_substring(line, ' ')
+		local person1 = words[1]
+		local person2 = words[11]:sub(1, -2)
+		local happiness = tonumber(words[4])
+		if happiness_table[person1] == nil then
+			happiness_table[person1] = {}
+		end
+
+		if words[3] == "gain" then
+			happiness_table[person1][person2] = happiness
+		else
+			happiness_table[person1][person2] = -1 * happiness
+		end
+		happiness_table[person1]['me'] = 0
+		happiness_table['me'][person1] = 0
+	end
+	local nr_to_name = {}
+	local i = 1
+	for k, _ in pairs(happiness_table) do
+		nr_to_name[i] = k
+		i = i + 1
+	end
+
+	local max_happiness = 0
+	for p in permutations(u.table_length(happiness_table)) do
+		local happiness = get_happiness_sum(happiness_table, p, nr_to_name)
+		if happiness > max_happiness then
+			max_happiness = happiness
+		end
+	end
+
+	print('13b:', max_happiness)
+end
+
+a()
+b()
